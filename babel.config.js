56c38b66b6c21533eb716149e38d3a802d4f1255
Justas Palumickas <jpalumickas@gmail.com>
2022-01-25 21:19:19 +0200

    Allow to use env variables

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,21 @@
+const path = require('path');
+
+const environment = process.env.ENVIRONMENT || 'development';
+const envPath = path.join(__dirname, `.env.${environment}`);
+require('dotenv').config({ path: envPath });
+
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    'transform-inline-environment-variables',
+    [
+      'module-resolver',
+      {
+        root: ['./src'],
+        alias: {
+          '~': './src',
+        },
+      },
+    ],
+  ],
 };
