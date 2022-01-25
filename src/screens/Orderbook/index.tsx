a49fbc05f771e6b3527c08310242bfcfe4701aeb
Justas Palumickas <jpalumickas@gmail.com>
2022-01-25 21:27:58 +0200

    Starting to add redux

---
+++ src/screens/Orderbook/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {
+  Text,
+  View,
+} from 'react-native';
+
+export const Orderbook = () => {
+  return (
+    <View>
+      <Text>Orderbook</Text>
+    </View>
+  )
+}
